0c0e277510858401d9d149bf1ed8b7a24a7c198c
Nicholas Mascia <nmmascia@gmail.com>
2016-09-17 10:09:52 -0400

    Add status route and cors.

--- src/application.js
+++ src/application.js
@@ -1,13 +1,18 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const routes = require('./yeezy-me/routes');
-app.use('/yeezy-me', routes);
+const yeezyRoutes = require('./yeezy-me/routes');
+const statusRoutes = require('./status/routes');
+
+app.use('/yeezy-me', yeezyRoutes);
+app.use('/status', statusRoutes);
 
 app.use((err, req, res, next) => {
     res.status(500).send({ error: err.name });
---
+++ src/status/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+router.get('/', (req, res, next) => {
+    res.send({ status: 'ok' });
+});
+
+module.exports = router;
